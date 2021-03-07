cdf8af5fe1916e8bd329847f7ea6ef62108b02ca
M.H-H <Melanie.Holzapfel-Huettenrauch@Student.HTW-Berlin.de>
2021-03-07 08:16:26 +0100

    added styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
+import 'semantic-ui-css/semantic.min.css'
 import '../styles/globals.css'
+import '../styles/app.css'
+
+import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Layout><Component {...pageProps} /></Layout>
 }
 
 export default MyApp
